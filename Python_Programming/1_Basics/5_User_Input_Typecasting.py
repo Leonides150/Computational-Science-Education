0b43f3b01d82f57e67229dbac27edea134422cbd
Leonides150 <96303077+Leonides150@users.noreply.github.com>
2026-04-29 19:50:17 -0500

    Delete Python_Programming/1_Basics directory

--- Python_Programming/1_Basics/5_User_Input_Typecasting.py
+++
@@ -1,9 +0,0 @@
-# The input function is used to take input from the user. It always returns a string.
-
-num1 = input('Enter the first number: ')
-num2 = input('Enter the second number: ')
-
-print(type(num1))
-
-print(num1 + num2)
-print(int(num1) + int(num2))
